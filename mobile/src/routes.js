fde6b469e0e04bda1c9cf883765bfdd844cf6b5b
guibyui <you@example.com>
2020-06-25 23:40:45 -0600

    Mobile app ready

---
+++ mobile/src/routes.js
@@ -0,0 +1,36 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: "Find a Dev",
+        },
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: "GitHub Profile",
+        },
+      },
+    },
+    {
+      defaultNavigationOptions: {
+        headerBackTitleVisible: false,
+        headerTintColor: "#FFF",
+        headerStyle: {
+          backgroundColor: "#7D40E7",
+          //   color: "#FFF",
+        },
+      },
+    }
+  )
+);
+
+export default Routes;
